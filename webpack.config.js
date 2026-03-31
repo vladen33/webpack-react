8e55cb2a52b3df94aa6213f85f590181a703a6ad
Vladislav Vasilev <vladen33@gmail.com>
2026-03-31 22:45:11 +0300

    Update 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
-const path = require('path');
-// Импортируем пакет path
+const path = require('path'); // для того чтобы превратить относительный путь в абсолютный, мы будем использовать пакет path
+const HTMLWebpackPlugins = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './path/to/my/entry/file.js',
+  entry: path.resolve(__dirname, './src/index.ts'), // точка входа в наше приложение содержит абсолютный путь к index.ts
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'), // путь, куда будет собираться наш проект
+    filename: 'main.js', // имя нашего бандла,
+    clean: true, // очистка папки перед сборкой
   },
+  // Нужно помочь вебпаку научиться работать  с jsx- и tsx-файлами, для этого используют ts-loader
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.[tj]sx?$/, // содержит регулярное выражение, которое содержит информацию, какие файлы должны обрабатываться этим loader'ом
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ], // для того чтобы ts-loader корректно отработал, нам нужен tsconfig, его можно создать вручную, а можно создать автоматически
+        /** чтобы проинициализировать его автоматически, можно установить пакет typesctipt глобально или использовать npx,
+         выполнив команду npx tsc --init. После создания конфига нужно включить "allowJs": true, чтобы работать не только c typescript,
+         также меняем "jsx": "react", чтобы мы могли работать с react-компонентами, и включаем карту ресурсов "sourceMap": true,
+         пока на этом всё, вернёмся в этот конфиг позже */
         exclude: /node_modules/,
-        use: [ ],
       },
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'], // указываем файлы, с которыми будет работать webpack
   },
-
-  devServer: {
-    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-    open: true, // сайт будет открываться сам при запуске npm run dev
-    hot: true,
-  },
+  plugins: [
+    new HTMLWebpackPlugins({
+      template: path.resolve(__dirname, 'public/index.html')
+    }),
+  ]
 };
-
-//module.exports — это синтаксис экспорта в Node.js
