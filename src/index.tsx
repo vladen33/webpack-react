f42ebce648f346f5a347ba4961636b20910b7c9e
Vladislav Vasilev <vladen33@gmail.com>
2026-04-04 11:05:51 +0300

    Final 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
+import style from './index.module.scss'
+import './index.scss'
+import './assets/images/cat.jpg'
+import { ReactComponent as ReactLogo } from './assets/images/react.svg'
 
-import style from './index.module.scss';
-import './index.scss';
-import './assets/images/cat.jpg';
-import { ReactComponent as ReactLogo } from './assets/images/react.svg';
+console.log(style) //использование объекта style важно для попадания стилей в css-бандл
 
-console.log(style); //использование объекта style важно для попадания стилей в css-бандл
-
-const numbers:number[] = [2, 3, 5];
+const numbers: number[] = [2, 3, 5]
 
 // Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
+const doubledNumbers = numbers.map((number) => number * 2)
 
-console.log(doubledNumbers); // 4, 6, 10 
+console.log(doubledNumbers) // 4, 6, 10
